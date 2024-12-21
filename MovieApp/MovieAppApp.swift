3963dfe289f2a811d2cc9e326d8b8389beb56c17
SergeyZakurakin <pricezak@gmail.com>
2024-12-20 20:20:36 -0500

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Sergey Zakurakin on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
