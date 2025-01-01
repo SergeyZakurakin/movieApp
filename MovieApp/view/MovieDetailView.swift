4d1aa68821678f0aab41760d56c072f7b9f38740
SergeyZakurakin <pricezak@gmail.com>
2025-01-01 18:35:46 -0500

    update

--- MovieApp/view/MovieDetailView.swift
+++ MovieApp/view/MovieDetailView.swift
@@ -114,26 +114,4 @@ struct MovieDetailView: View {
 }
 
 
-struct CastView: View {
-    
-    let cast: CastProfile
-    
-    var body: some View {
-        VStack {
-            
-            AsyncImage(url: cast.photoURL) { image in
-                image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 100, height: 120)
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-            } placeholder: {
-                ProgressView()
-                    .frame(width: 100, height: 120)
-            }
-            Text(cast.name)
-//                .frame(width: 100)
-//                .lineLimit(0)
-        }
-    }
-}
+
